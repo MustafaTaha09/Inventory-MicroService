e1a1507a133316b6703a3b66c714f949c6413e0b
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-05-05 15:35:33 +0300

    integration of eureka service registry with gprc

--- src/main/java/com/example/inventory/grpc/InventoryGrpcServiceImpl.java
+++ src/main/java/com/example/inventory/grpc/InventoryGrpcServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.inventory.grpc; // Create a suitable package (e.g., grpc)
 
+import com.example.inventory.InventoryGrpcServiceGrpc;
+import com.example.inventory.ProductExistenceRequest;
+import com.example.inventory.ProductExistenceResponse;
 import com.example.inventory.service.InventoryService; // Your existing business logic service
-import com.example.grpc.inventory.*; // Import generated classes from proto (adjust package if needed)
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
